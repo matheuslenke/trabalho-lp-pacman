ee24d89d7a08a51870092b355d5bfbddcbd80af7
fernando0x40 <fernando0x40@gmail.com>
2021-04-02 02:55:19 -0300

    Adicionei os fantasmas e limpei o código

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,13 +2,26 @@ import Phaser from 'phaser'
 
 import FoodImg from '../../assets/images/other/apple.png'
 import PacmanSprite from '../../assets/images/pacman.png'
+import BlinkySprite from '../../assets/images/Sprite_Sheets/blinky.png'
+import PinkySprite from '../../assets/images/Sprite_Sheets/pinky.png'
+import InkySprite from '../../assets/images/Sprite_Sheets/inky.png'
+import ClydeSprite from '../../assets/images/Sprite_Sheets/clyde.png'
 
 import Map from '../../assets/images/map.png'
 
 import Pacman from '../classes/Pacman'
 import Food from '../classes/Food'
+import Fantasma from '../classes/Fantasma'
+import Blinky from '../classes/Blinky'
+import Pinky from '../classes/Pinky'
+import Inky from '../classes/Inky'
+import Clyde from '../classes/Clyde'
 
 let pacman
+let blinky
+let pinky
+let inky
+let clyde
 let platforms
 
 export default class extends Phaser.Scene {
@@ -21,6 +34,22 @@ export default class extends Phaser.Scene {
         this.load.spritesheet('pacman', PacmanSprite, {
             frameWidth: 26,
             frameHeight: 26,
+        })
+        this.load.spritesheet('blinky', BlinkySprite, {
+            frameWidth: 16,
+            frameHeight: 16,
+        })
+        this.load.spritesheet('pinky', PinkySprite, {
+            frameWidth: 16,
+            frameHeight: 16,
+        })
+        this.load.spritesheet('inky', InkySprite, {
+            frameWidth: 16,
+            frameHeight: 16,
+        })
+        this.load.spritesheet('clyde', ClydeSprite, {
+            frameWidth: 16,
+            frameHeight: 16,
         })
         this.load.image('map', Map)
     }
@@ -35,8 +64,16 @@ export default class extends Phaser.Scene {
         platforms.create(400, 568, 'food').setScale(10).refreshBody()
 
         pacman = new Pacman(this, 15, 8)
+        blinky = new Blinky(this)
+        pinky = new Pinky(this)
+        inky = new Inky(this)
+        clyde = new Clyde(this)
 
         this.physics.add.collider(pacman.getBody(), platforms)
+        this.physics.add.collider(blinky.getBody(), platforms)
+        this.physics.add.collider(pinky.getBody(), platforms)
+        this.physics.add.collider(inky.getBody(), platforms)
+        this.physics.add.collider(clyde.getBody(), platforms)
         this.cursors = this.input.keyboard.createCursorKeys()
     }
 
@@ -57,74 +94,11 @@ export default class extends Phaser.Scene {
             console.log('Facing Down')
             pacman.faceDown()
         }
-
-        // if (this.pacman.update(time))
-        // {
-        //     //  If the this.pacman updated, we need to check for collision against food
-
-        //     if (this.pacman.collideWithFood(this.food))
-        //     {
-        //         // repositionFood();
-        //     }
-        // }
+        if (time % 400 >= 0 && time % 400 <= 15) {
+            clyde.cycleDirection();
+            pinky.cycleDirection();
+            blinky.cycleDirection();
+            inky.cycleDirection();
+        }
     }
-
-    /**
-     * We can place the food anywhere in our 40x30 grid
-     * *except* on-top of the snake, so we need
-     * to filter those out of the possible food locations.
-     * If there aren't any locations left, they've won!
-     *
-     * @method repositionFood
-     * @return {boolean} true if the food was placed, otherwise false
-     */
-    //  repositionFood ()
-    // {
-    //     //  First create an array that assumes all positions
-    //     //  are valid for the new piece of food
-
-    //     //  A Grid we'll use to reposition the food each time it's eaten
-    //     const testGrid = [];
-
-    //     for (var y = 0; y < 30; y++)
-    //     {
-    //         testGrid[y] = [];
-
-    //         for (var x = 0; x < 40; x++)
-    //         {
-    //             testGrid[y][x] = true;
-    //         }
-    //     }
-
-    //     snake.updateGrid(testGrid);
-
-    //     //  Purge out false positions
-    //     const validLocations = [];
-
-    //     for (var y = 0; y < 30; y++)
-    //     {
-    //         for (var x = 0; x < 40; x++)
-    //         {
-    //             if (testGrid[y][x] === true)
-    //             {
-    //                 //  Is this position valid for food? If so, add it here ...
-    //                 validLocations.push({ x, y });
-    //             }
-    //         }
-    //     }
-
-    //     if (validLocations.length > 0)
-    //     {
-    //         //  Use the RNG to pick a random food position
-    //         const pos = Phaser.Math.RND.pick(validLocations);
-
-    //         //  And place it
-    //         food.setPosition(pos.x * 16, pos.y * 16);
-
-    //         return true;
-    //     }
-
-    //         return false;
-
-    // }
 }
