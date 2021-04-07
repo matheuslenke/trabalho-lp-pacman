3631c38381378561679ce749c1bfa771d0c25e5d
fernando0x40 <fernando0x40@gmail.com>
2021-04-07 12:21:38 -0300

    adicionei método getter pras direções

--- src/classes/Pacman.js
+++ src/classes/Pacman.js
@@ -84,6 +84,18 @@ export default new Phaser.Class({
     getNextDirection() {
         return this.nextDirection
     },
+    directionUp() {
+        return UP;
+    },
+    directionDown() {
+        return DOWN;
+    },
+    directionLeft() {
+        return LEFT;
+    },
+    directionRight() {
+        return RIGHT;
+    },
     wantToFaceLeft() {
         this.nextDirection = LEFT
     },
